886c92eb377c028b33aec26faa34cf6ac6dd73df
Aleksander <sanyok.10rus@gmail.com>
2023-08-12 01:52:41 +0300

    proj created

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './shared/styles/_common.scss';
+import style from './App.module.scss';
+
+function App() {
+
+  return (
+    <div className={style.app}>
+      <div className={style.container}>
+
+      </div>
+      <div className={style.container}>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/shared/mock/mp3Files.ts
@@ -0,0 +1,20 @@
+export const mp3Data = [
+  {
+    id: 1,
+    name: 'God Was Never On Your Side',
+    author: 'Motorhead',
+    src: '../../assets/mp3/Motorhead - God Was Never On Your Side.mp3'
+  },
+  {
+    id: 2,
+    name: 'Toxic',
+    author: 'Boywithuke',
+    src: '../../assets/mp3/Boywithuke - Toxic.mp3'
+  },
+  {
+    id: 3,
+    name: 'Moment (feat. Paulwetz)',
+    author: 'Dillistone',
+    src: '../../assets/mp3/Dillistone - Moment (feat. Paulwetz).mp3'
+  },
+]
